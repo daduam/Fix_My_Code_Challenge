764e11d13a118563060b1b061b3a4c573a3bb153
Joseph Ampadu <jkampadujnr@gmail.com>
2023-05-05 22:43:14 +0000

    Fix issues with Square class

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square():
+    """Defines a Square"""
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a square"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        """Perimeter of the square"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """String representation"""
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
